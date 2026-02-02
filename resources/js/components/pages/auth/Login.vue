66e98cc6b65b58a6fac4fce7a78ef274261f1e4a
Alec Gordon <insanealec@gmail.com>
2026-02-02 07:56:17 -0500

    Clear errors on mounting component

--- resources/js/components/pages/auth/Login.vue
+++ resources/js/components/pages/auth/Login.vue
@@ -1,7 +1,12 @@
 <script setup>
+import { onMounted } from "vue";
 import { useAuthStore } from "../../../stores/auth";
 
 const authStore = useAuthStore();
+
+onMounted(() => {
+    authStore.errors = {};
+});
 </script>
 <template>
     <div
