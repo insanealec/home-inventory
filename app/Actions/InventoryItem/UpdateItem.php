b7e0398d63d3c65ab9bf3383116d2f68a8793612
Alec Gordon <insanealec@gmail.com>
2026-02-16 09:04:29 -0500

    Add update item action

---
+++ app/Actions/InventoryItem/UpdateItem.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Actions\InventoryItem;
+
+use App\Models\InventoryItem;
+use Illuminate\Http\Request;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class UpdateItem
+{
+    use AsAction;
+
+    public function handle(InventoryItem $item, array $data)
+    {
+        return $item->update($data) ? $item : null;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'id' => 'required|exists:inventory_items,id',
+            'name' => 'required|string|max:255',
+            'sku' => 'nullable|string|max:100',
+            'description' => 'nullable|string',
+            'quantity' => 'required|integer|min:0',
+            'stock_location_id' => 'nullable|exists:stock_locations,id',
+            'position' => 'nullable|string|max:255',
+        ];
+    }
+
+    public function asController(Request $request, InventoryItem $item)
+    {
+        return $this->handle($item, $request->all());
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Actions\InventoryItem\CreateItem;
 use App\Actions\InventoryItem\DeleteItem;
 use App\Actions\InventoryItem\LoadItem;
 use App\Actions\InventoryItem\LoadItems;
+use App\Actions\InventoryItem\UpdateItem;
 use App\Actions\Token\CreateToken;
 use App\Actions\Token\DestroyToken;
 use App\Actions\Token\LoadTokens;
@@ -22,4 +23,5 @@ Route::delete('/tokens/{tokenId}', DestroyToken::class)->middleware('auth:sanctu
 Route::get('/inventory-items', LoadItems::class)->middleware('auth:sanctum');
 Route::post('/inventory-items', CreateItem::class)->middleware('auth:sanctum');
 Route::get('/inventory-items/{inventoryItem}', LoadItem::class)->middleware(['auth:sanctum', UserOwnsResource::class]);
+Route::put('/inventory-items/{inventoryItem}', UpdateItem::class)->middleware(['auth:sanctum', UserOwnsResource::class]);
 Route::delete('/inventory-items/{inventoryItem}', DeleteItem::class)->middleware(['auth:sanctum', UserOwnsResource::class]);
